f936f5612399033644c483d784a65ab813c9c173
ShadmanA <shadman.ahmed.0202@gmail.com>
2026-04-20 03:49:07 -0400

    Number functions and comments

--- white_strip.py
+++ white_strip.py
@@ -1,2 +1,4 @@
+# By Shadman Ahmed
+# This program removes white space from a string by using the strip functions
 full_name = "     John Dingle     "
 print(f"{full_name}\n{full_name.strip()}\n\t{full_name.strip()}\n{full_name.rstrip()}\n{full_name.lstrip()}")

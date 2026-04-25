83f70d5f581992e7454a201af432539f3c312fcc
ShadmanA <shadman.ahmed.0202@gmail.com>
2026-04-25 12:44:33 -0400

    List sorting operations

---
+++ ch_3/see_world.py
@@ -0,0 +1,15 @@
+visit_list = ['Switzerland', 'Japan', 'Maldives', 'Singapore', 'Italy']
+print(visit_list)
+print(sorted(visit_list))
+print(visit_list)
+print(sorted(visit_list, reverse=True))
+print(visit_list)
+visit_list.reverse()
+print(visit_list)
+visit_list.reverse() #Function with no return value
+print(visit_list)
+visit_list.sort() #Function with no return value
+print(visit_list)
+visit_list.sort(reverse=True) #Function with no return value
+print(visit_list)
+
